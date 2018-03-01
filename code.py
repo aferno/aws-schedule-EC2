2782a430febaf7ac1b89c29713409a696ba1701b
afernj <afern.ru@gmail.com>
2018-03-01 18:33:38 +0300

    Fixed

--- code.py
+++ code.py
@@ -2,5 +2,13 @@ import boto3
 
 def lambda_handler(event, context):
     ec2 = boto3.resource('ec2')
-    ec2.instances.filter(InstanceIds=["i-0eb6cb8607c458bfd"]).stop()
-    print("Complete")
+    machine_names = []
+    running_instance = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+    for instance in running_instance:
+        for tag in instance.tags:
+            if tag['Key'] == 'env' and tag['Value'] == 'test':
+                machine_names.append(instance.instance_id)
+                instance.stop()
+    #Return ID
+    return machine_names
